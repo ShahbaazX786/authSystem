2a7d312840252f42b4635aba8d7c97299bd82394
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 09:41:21 +0530

    The connect call is async. and added optional chaining.

--- Backend/db/ConnectDB.js
+++ Backend/db/ConnectDB.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const connectionStatus = mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Sucessfully Connected :${connectionStatus.connection.host}`);
+        const connectionStatus = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Sucessfully Connected :${connectionStatus?.connection?.host}`);
     } catch (error) {
         console.error('Error Connecting To DB:', error.message);
         process.exit(1);
