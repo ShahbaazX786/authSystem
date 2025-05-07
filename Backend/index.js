5c21e37857e9fb71fbf038f17e3ce98bd1372e45
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 18:30:53 +0530

    utilized express json middle ware to support json payloads in post requests.

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,19 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import { connectDB } from './db/ConnectDB.js';
-import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
     res.send('Bismillah');
 });
-
-app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     connectDB();
