f6e98b9fa4fc6538a2be204ca4b0d336e0dbe50d
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 17:12:57 +0530

    Updated logging.

--- Backend/db/ConnectDB.js
+++ Backend/db/ConnectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const connectionStatus = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Sucessfully Connected :${connectionStatus?.connection?.host}`);
+        console.log(`Sucessfully Connected to DB at :${connectionStatus?.connection?.host}`);
     } catch (error) {
         console.error('Error Connecting To DB:', error.message);
         process.exit(1);
