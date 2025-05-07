44ddff3efc3a0d958309c3ce2360247e9073a568
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 19:00:58 +0530

    Optimized model creation logic.

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcryptjs from 'bcryptjs';
-import { User } from '../models/user.model.js';
+import User from '../models/user.model.js';
 import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
 import generateVerificationToken from '../utils/generateVerificationToken.js';
 import { get24HoursInMilliseconds } from '../utils/getSimpleThings.js';
