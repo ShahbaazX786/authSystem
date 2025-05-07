335268cb08850925aae60eadd3b12fd2857fa3a4
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 19:55:09 +0530

    updated routes.

--- Backend/Routes/auth.route.js
+++ Backend/Routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { DeleteUser, LogIn, LogOut, SignUp } from '../controllers/auth.controller.js';
+import { DeleteUser, LogIn, LogOut, refreshToken, SignUp } from '../controllers/auth.controller.js';
 
 const Router = express.Router();
 
 Router.post('/signup', SignUp);
 Router.post('/login', LogIn);
 Router.post('/logout', LogOut);
+Router.post('/refreshToken', refreshToken);
 Router.post('/deleteUser', DeleteUser);
 
 export default Router;
