612dd000b8e96212ad55c2d76a7d7f5ea8490f2e
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 17:30:16 +0530

    Updated other controller methods.

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,3 +1,9 @@
 export const SignUp = async (req, res) => {
     res.send('Signup Route');
 }
+export const LogIn = async (req, res) => {
+    res.send("Login Page");
+}
+export const LogOut = async (req, res) => {
+    res.send("Logout Page");
+}
