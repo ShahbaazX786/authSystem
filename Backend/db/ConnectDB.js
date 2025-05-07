4cb5ecce74ac65c73d45c84866de3c7b33734623
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 09:17:39 +0530

    added a temp connectDB method.

---
+++ Backend/db/ConnectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const connectionStatus = mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Sucessfully Connected :${connectionStatus.connection.host}`);
+    } catch (error) {
+        console.error('Error Connecting To DB:', error.message);
+        process.exit(1);
+    }
+}
