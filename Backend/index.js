e63d109880e0dbeb0d74803b64ddaeff084fef37
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 09:17:29 +0530

    configuring dotenv.

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import { connectDB } from './db/ConnectDB.js';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = process.env.PORT || 3000;
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('Bismillah');
 });
 
-
 app.listen(port, () => {
+    connectDB();
     console.log(`Server is running on port ${port}`)
 })
