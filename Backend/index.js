740977434f3cd29a71b0512a129c8ab21a481de6
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 17:13:07 +0530

    imported and utilized routes.

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { connectDB } from './db/ConnectDB.js';
 import dotenv from 'dotenv';
+import authRoutes from './Routes/auth.route.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,8 @@ dotenv.config();
 app.get('/', (req, res) => {
     res.send('Bismillah');
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
     connectDB();
