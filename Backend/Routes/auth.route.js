985b0e598636b1e049e5b2ebe585037ef1c71f1f
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-07 18:30:12 +0530

    updated method types.

--- Backend/Routes/auth.route.js
+++ Backend/Routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { LogIn, LogOut, SignUp } from '../controllers/auth.controller';
+import { LogIn, LogOut, SignUp } from '../controllers/auth.controller.js';
 
 const Router = express.Router();
 
-Router.get('/signup', SignUp);
-Router.get('/login', LogIn);
-Router.get('/logout', LogOut);
+Router.post('/signup', SignUp);
+Router.post('/login', LogIn);
+Router.post('/logout', LogOut);
 
 export default Router;
