0372e0619f461da55279ef639ac3834444f0886c
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-09 16:28:52 +0530

    Updated method name and otp time to 5 minutes.

--- Backend/utils/getSimpleThings.js
+++ Backend/utils/getSimpleThings.js
@@ -1,5 +1,5 @@
-const get24HoursInMilliseconds = () => {
-    return Date.now() + 24 * 60 * 60 * 1000; // returns a validity of 24Hours from current timedate.
+const getVerificationTokenExpireTime = () => {
+    return Date.now() + 5 * 60 * 1000; // returns a validity of 5 minutes from current timedate.
 }
 
-export { get24HoursInMilliseconds };
+export { getVerificationTokenExpireTime };
