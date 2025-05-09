13d2131ed65c882ff208debc6c2ace8c09f8be70
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-09 12:47:48 +0530

    Updated index.js with dotenv config import

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,16 @@
-import dotenv from 'dotenv';
+import './utils/dotenvConfig.js';
 import express from 'express';
 import { connectDB } from './db/ConnectDB.js';
 import authRoutes from './routes/auth.route.js';
 
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.send('Bismillah');
 });
 
