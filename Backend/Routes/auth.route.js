21d992c7ab4f6af40b43d7ace607f50e3b3f92ee
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-09 16:28:24 +0530

    Added two more routes.

--- Backend/Routes/auth.route.js
+++ Backend/Routes/auth.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { DeleteUser, LogIn, LogOut, refreshToken, SignUp } from '../controllers/auth.controller.js';
+import { DeleteUser, LogIn, LogOut, RefreshToken, SignUp, VerifyEmail, VerifyOTP } from '../controllers/auth.controller.js';
 
 const Router = express.Router();
 
 Router.post('/signup', SignUp);
 Router.post('/login', LogIn);
 Router.post('/logout', LogOut);
-Router.post('/refreshToken', refreshToken);
+Router.post('/refreshToken', RefreshToken);
 Router.post('/deleteUser', DeleteUser);
+Router.post('/verifyEmail', VerifyEmail);
+Router.post('/verifyOtp', VerifyOTP);
 
 export default Router;
