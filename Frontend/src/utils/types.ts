e75c42cf34bcead5b9ae20e9ed26ea753138b614
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-20 18:16:58 +0530

    Added new types.

--- Frontend/src/utils/types.ts
+++ Frontend/src/utils/types.ts
@@ -5,3 +5,11 @@ export type FloatingShapePropType = {
   left: string;
   delay: number;
 };
+
+export type InputPropType = {
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+} & any;
+
+export type ChangeEventType = React.ChangeEvent<HTMLInputElement>;
+export type FormEventType = React.FormEvent<HTMLFormElement>;
