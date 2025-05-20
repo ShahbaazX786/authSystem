c80bc45eaa86f2995c4b53ea322a778f00f5e4ca
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-20 17:40:03 +0530

    Added a bg with floating shapes.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,33 @@
+import FloatingShapes from "./components/FloatingShape";
 import "./index.css";
 
 function App() {
-  return <div className="text-8xl bg-green-500 font-bold">Bismillah</div>;
+  // return <div className="text-8xl bg-green-500 font-bold">Bismillah</div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShapes
+        color="bg-green-500"
+        size="w-64 h-64"
+        top={"-5%"}
+        left={"10%"}
+        delay={0}
+      />
+      <FloatingShapes
+        color="bg-emarald-500"
+        size="w-48 h-48"
+        top={"70%"}
+        left={"80%"}
+        delay={5}
+      />
+      <FloatingShapes
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top={"40%"}
+        left={"-10%"}
+        delay={2}
+      />
+    </div>
+  );
 }
 
 export default App;
