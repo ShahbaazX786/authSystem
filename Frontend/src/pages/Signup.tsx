ed936fb21777c4c44d57b67bccec078145065ad6
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-05-21 17:18:05 +0530

    Utilized passwordstrengthchecker.

--- Frontend/src/pages/Signup.tsx
+++ Frontend/src/pages/Signup.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import Input from "../components/Input";
 import type { ChangeEventType, FormEventType } from "../utils/types";
+import PasswordStrengthChecker from "../components/PasswordStrengthChecker";
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -46,6 +47,8 @@ const Signup = () => {
             value={password}
             onChange={(e: ChangeEventType) => setPassword(e.target.value)}
           />
+
+          <PasswordStrengthChecker password={password} />
           <motion.button
             className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
             whileHover={{ scale: 1.05 }}
