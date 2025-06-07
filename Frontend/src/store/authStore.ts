da18bb958fa8fc66005c58e37d000fd01e3b128a
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-06-07 19:03:13 +0530

    created a zustand store + api handler.

---
+++ Frontend/src/store/authStore.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { create } from "zustand";
+
+const API_URL = "http://localhost:7860/api/auth";
+axios.defaults.withCredentials = true;
+export const useAuthStore = create((set) => ({
+  user: null,
+  isAuthenticated: false,
+  error: null,
+  isLoading: false,
+  isCheckingAuth: false,
+
+  signup: async (email, password, name) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/signup`, {
+        email,
+        password,
+        name,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error Signing Up",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
+}));
