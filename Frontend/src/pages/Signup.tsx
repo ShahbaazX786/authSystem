68cf30f03c3fdbe8513b9cf7952996e5d6aef073
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-06-08 10:37:36 +0530

    Minor bug fix.

--- Frontend/src/pages/Signup.tsx
+++ Frontend/src/pages/Signup.tsx
@@ -56,7 +56,7 @@ const Signup = () => {
             value={password}
             onChange={(e: ChangeEventType) => setPassword(e.target.value)}
           />
-          {error && <p className="text-red-500 font-semibold mt-2">{error}s</p>}
+          {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
           <PasswordStrengthChecker password={password} />
           <motion.button
             className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
