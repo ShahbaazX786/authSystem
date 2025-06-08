fdec70ac1089c03cf9659f17620d4ad9e36350e9
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-06-08 11:32:15 +0530

    Added new store method for verifying otp.

--- Frontend/src/store/authStore.ts
+++ Frontend/src/store/authStore.ts
@@ -31,4 +31,22 @@ export const useAuthStore = create((set) => ({
       throw error;
     }
   },
+
+  verifyEmail: async (otp: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/verifyOtp`, { otp });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error: any) {
+      set({
+        error: error?.response?.data?.message || "Error Verifying Email",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
 }));
