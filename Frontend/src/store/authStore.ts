cfa412cc95f69883fa2e458df80c6acc4d3954fc
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-06-08 12:31:39 +0530

    Added new checkAuth store method.

--- Frontend/src/store/authStore.ts
+++ Frontend/src/store/authStore.ts
@@ -49,4 +49,22 @@ export const useAuthStore = create((set) => ({
       throw error;
     }
   },
+
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/check-auth`);
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isCheckingAuth: false,
+      });
+    } catch (error: any) {
+      set({
+        error: null,
+        isAuthenticated: false,
+        isCheckingAuth: false,
+      });
+    }
+  },
 }));
