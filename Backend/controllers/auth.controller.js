59361dfdd0d80b4bbf2283f7c128b3e15c4ce516
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-06-09 13:16:54 +0530

    added a checkauth controller.

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -198,3 +198,15 @@ export const ResetPassword = async (req, res) => {
         return res.status(500).json({ success: false, message: "Something went wrong." });
     }
 }
+
+export const CheckAuth = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId).select('-password');
+        if (!user) return res.status(400).json({ success: false, message: 'User not found' })
+        res.status(200).json({ success: true, user });
+    } catch (error) {
+        console.log('Error in Checking User Authentication', error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+
+}
