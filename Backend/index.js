5cb8f97a2b0eaf3386a0588ff20e6789008aef0d
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-06-09 13:16:43 +0530

    configured cookieparset to check authentication based on req cookies.

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,14 @@ import express from 'express';
 import { connectDB } from './db/ConnectDB.js';
 import authRoutes from './routes/auth.route.js';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 
 app.get('/', (_req, res) => {
